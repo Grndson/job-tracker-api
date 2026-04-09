63704d4b647ea073616492501fbd47486cc4a65e
Papafrost <vukoedmund00@gmail.com>
2026-04-09 20:21:36 +0300

    fix cors for deployment stage

--- config/cors.php
+++ config/cors.php
@@ -18,20 +18,20 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        env('FRONTEND_URL', 'http://localhost:5173'),
-    ],
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+],
 
-    'allowed_origins_patterns' => [
-        // Allow any Vercel preview URL for the project
-        '#^https://.*\.vercel\.app$#',
-    ],
+'allowed_origins_patterns' => [
+    '#^https://.*\.vercel\.app$#',
+],
+
+'supports_credentials' => false,
 
     'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
     'max_age' => 0,
-
-    'supports_credentials' => true,
 
 ];
